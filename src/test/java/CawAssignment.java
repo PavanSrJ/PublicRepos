9fd1cf6b7da314a001a4912d9e509da3662e4afe
PavansrM <143777548+PavansrM@users.noreply.github.com>
2024-01-09 23:22:26 +0530

    Working Commit

--- src/test/java/CawAssignment.java
+++ src/test/java/CawAssignment.java
@@ -1,4 +1,3 @@
-//import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,10 +7,6 @@ import java.util.List;
 public class CawAssignment {
 
     public static void main(String[] args) {
-        // Set the property for the Chrome driver
-        // Make sure to replace 'path/to/chromedriver' with the actual path to the chromedriver executable
-        //System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-    	//webDriverManager.chromeDriver().setup()	
     	
         WebDriver driver = new ChromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
@@ -35,10 +30,24 @@ public class CawAssignment {
         WebElement refreshButtonElement = driver.findElement(By.xpath("//button[@id='refreshtable']"));
         refreshButtonElement.click();
         
-        List<WebElement> allHeaders = driver.findElements(By.xpath("//table[@id='dynamictable']//tr"));
-        
-        System.out.println(allHeaders.size());
+        List<WebElement> tableRows = driver.findElements(By.xpath("//table[@id='dynamictable']//tr"));
+        String actualJsonData = captureDataFromTable(tableRows);
+                
+        // Assert that the captured data matches the expected JSON data
+        assert actualJsonData.equals(jsonTableData) : "Data in the UI table does not match the expected JSON data.";
         
         driver.quit();
+    }
+    
+    private static String captureDataFromTable(List<WebElement> tableRows) {
+        StringBuilder capturedData = new StringBuilder();
+        System.out.println();
+        for (WebElement row : tableRows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));          
+            for (WebElement cell : cells) {
+                capturedData.append(cell.getText()).append(",");
+            }
+        }
+        return capturedData.toString();
     }
 }
